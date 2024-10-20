a978f3aa0f845a439e8651e73e2114861eadf167
Valentyn Berekelia <m903127m@gmail.com>
2024-10-20 23:03:47 +0300

    новлення lab01 з новими змінами

--- script.js
+++ script.js
@@ -1,9 +1,37 @@
-const image = document.getElementById("img1");
-const button = document.getElementById("button1");
-const colors = ['red', 'green', 'blue', 'purple', 'orange'];
+const image1 = document.getElementById("img1");
+const image2 = document.getElementById("img2");
+const image3 = document.getElementById("img3");
+const button1 = document.getElementById("button1");
+button1.addEventListener("click", () => {
+    image1.style.border = '3px solid red';
+});
+const button2 = document.getElementsByTagName("button")[1];
+button2.addEventListener("click", () => {
+    image2.style.border = '3px solid red';
+});
+const button3 = document.getElementsByClassName("button3")[0];
+button3.addEventListener("click", () => {
+    image3.style.border = '3px solid red';
+});
 
-button.addEventListener("click", () => {
+image1.addEventListener("mouseover", () => {
+    image1.style.transform = 'scale(1.1)';
+});
+image1.addEventListener("mouseout", () => {
+    image1.style.transform = 'scale(1)';
+});
 
-    const randomColor = colors[Math.floor(Math.random() * colors.length)];
-    image.style.borderColor = randomColor;
-})
+image2.addEventListener("mouseover", () => {
+    image2.style.transform = 'scale(1.1)';
+});
+image2.addEventListener("mouseout", () => {
+    image2.style.transform = 'scale(1)';
+});
+
+image3.addEventListener("mouseover", () => {
+    image3.style.transform = 'scale(1.1)';
+});
+image3.addEventListener("mouseout", () => {
+    image3.style.transform = 'scale(1)';
+});
+
